ef877b3992af8f025987ed894d52d7a2e141bd9b
Maria Helenius <maheleni@c2r4p3.hive.fi>
2024-05-15 19:36:06 +0300

    Changed logic of code, works in come cases

--- get_next_line.c
+++ get_next_line.c
@@ -12,63 +12,130 @@
 
 #include "get_next_line.h"
 
-//fix headerfile and utils
-int		ft_findchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *s);
+char	*join_rest_read(char *rest, char *read_buf)
+{
+	char		*new_rest;
+	size_t		rest_len;
+	size_t		read_len;
+
+	if (!rest || !read_buf)
+		return (NULL);
+	rest_len = ft_strlen(rest);
+	read_len = ft_strlen(read_buf);
+	new_rest = (char *) malloc((rest_len + read_len + 1) * sizeof (char));
+	if (new_rest == NULL)
+		return (NULL);
+	ft_strlcat(new_rest, rest, (rest_len + read_len + 1));
+	ft_strlcat(new_rest, read_buf, (rest_len + read_len + 1));
+	free(rest);
+	return (new_rest);
+}
+
+char	*read_to_rest(int fd, char *rest)
+{
+	char	*read_buf;
+	int		read_bytes;
+
+	//allocate read buffer
+	read_buf = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char));
+	//check that allocation worked
+	if (!read_buf)
+		return (NULL);
+	//while rest does not contain newline and bytes read is not 0
+	read_bytes = 1;
+	if (!rest)
+	{
+		rest = (char *) malloc(1 * sizeof(char));
+		rest[0] = '\0';
+	}
+	while (!ft_strchr(rest, '\n') && read_bytes != 0)
+	{
+		//read into buffer and save bytes read
+		read_bytes = read(fd, read_buf, BUFFER_SIZE);
+		//check that reading was successfull
+		if (read_bytes == -1)
+		{
+			//if not free the read buffer
+			free(read_buf);
+			return (NULL);
+		}
+		//nullterminate the readbuffer
+		read_buf[BUFFER_SIZE] = '\0';
+		//create the rest string by combining the rest and read buffer and freeing the old rest
+		rest = join_rest_read(rest, read_buf);
+	}
+	//now we have a new line in rest, so lets free the read buffer
+	free(read_buf);
+	return (rest);
+}
+
+char	*get_line_from_rest(char *rest)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!rest[i])
+		return (NULL);
+	while (rest[i] && rest[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (rest[i] && rest[i] != '\n')
+	{
+		line[i] = rest[i];
+		i++;
+	}
+	if (rest[i] == '\n')
+	{
+		line[i] = rest[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*get_new_rest(char *rest)
+{
+	char	*new_rest;
+	int		i;
+
+	i = 0;
+	while (rest[i] && rest[i] != '\n')
+		i++;
+	if (!rest[i])
+	{
+		free(rest);
+		return (NULL);
+	}
+	new_rest = (char *)malloc((ft_strlen(rest) - i + 1) * sizeof(char));
+	if (!new_rest)
+		return (NULL);
+	ft_strlcat(new_rest, rest + i + 1, (ft_strlen(rest) - i + 1));
+	free(rest);
+	return (new_rest);
+}
 
 char	*get_next_line(int fd)
 {
-	char		*read_buf;
-	static char	*rest = "\0";
+	static char	*rest;
 	char		*line;
-	int			line_end_i;
-	char		*temp;
-	ssize_t		bytes_read;
 
-	while (1)
-	{
-		read_buf = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char)); //allocate memery to read into + to null terminate
-		if (read_buf == NULL)
-			return (NULL);
-		bytes_read = read(fd, read_buf, BUFFER_SIZE);
-		if (bytes_read == 0)
-		{
-			line = ft_strdup(rest);
-			free(read_buf);
-			free(rest);
-			if (line == NULL)
-				return (NULL);
-			return (line);
-		}
-		else if (bytes_read == -1)
-		{
-			free(read_buf);
-			//free(rest);
-			return (NULL);
-		}
-		read_buf[BUFFER_SIZE] = '\0';
-		temp = ft_strjoin(rest, read_buf);
-		//free(rest);
-		free(read_buf);
-		if (temp == NULL)
-			return (NULL);
-		line_end_i = ft_findchr(temp, '\n');
-		if (line_end_i != -1)
-		{
-			line = ft_substr(temp, 0, line_end_i + 1);
-			if (line == NULL)
-			{
-				free(temp);
-				return (NULL);
-			}
-			rest = ft_strdup(temp + ft_strlen(line));
-			return (line);
-		}
-		rest = ft_strdup(temp);
-		free(temp);
-	}
-	return(NULL);
+	//check that fd is bigger than or equal to 0 and BUFFER_SIZE is bigger than 0
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	//create rest (rest is at this point everything read containing newline or end) with read_to_rest()
+	rest = read_to_rest(fd, rest);
+	//check that the creation of rest went well
+		//if not: return NULL
+	if (!rest)
+		return (NULL);
+	//get the next line into its own variable, nothing more
+	line = get_line_from_rest(rest);
+	//remove the next line from rest and create a new rest (the acctual rest), free everything else than the new rest
+	rest = get_new_rest(rest);
+	//return the line
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -21,14 +21,8 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
-
-int		ft_findchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
-size_t	ft_strlen(const char *s);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
 
 #endif
